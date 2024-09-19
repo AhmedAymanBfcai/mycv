9c9718db5d6397fce013092a0231400f7205038e
AhmedAymanBfcai <ahmedaymanbfcai123@gmail.com>
2024-09-19 14:00:30 +0300

    Adding signup controller with validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,17 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
     type: 'sqlite',
-    database: 'db.sqlite', // This cause typeorm throwho sqlite to create this file.
-    entities: [User],
-    synchronize: true
+    database: 'db.sqlite', // This cause typeorm throw sqlite to create this file.
+    entities: [User, Report],
+    synchronize: true //Only for use in dev mode. This option is going to cause TypeOrm to take a look at the structure of all you different entities and automatically update the structure of your database.
   }), UsersModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService],
-})
+}) 
+
 export class AppModule {}
